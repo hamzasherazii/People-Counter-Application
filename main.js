250547a1beac6c2229989e30875ac10eac233cf0
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-06-11 18:05:53 +0500

    the button increments now

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-// document.getElementById("count").innerText = 5
-
-// let count = 0
-
-// console.log(count)
 
 // initialize the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML file to reflect the new count 
 
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
+
 function increment(){
-    console.log("The button was clicked!")
-}
+    // console.log("The button was clicked!");
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count);
+};
